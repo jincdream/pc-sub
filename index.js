2515526fbcf90891c193cfc4a5d4d1b29af59186
Jin_C <275727449@qq.com>
2015-07-21 17:58:48 +0800

    npm publish
    npm publish

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //vi pc-sub/index.js
 var fis = module.exports =  require('fis3');
 
-fis.require.prefixes.unshift('foo');
-fis.cli.name = 'foo';
+fis.require.prefixes.unshift('pc-sub');
+fis.cli.name = 'pc-sub';
 fis.cli.info = require('./package.json');
