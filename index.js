daf234e7b044d746926e9da27c6bd316e0677742
Jin_C <275727449@qq.com>
2015-07-30 18:31:13 +0800

    upload
    upload

--- index.js
+++ index.js
@@ -26,18 +26,21 @@ function createRequireConfig(ret, conf, settings, opt){
     // console.log(file)
     var requires = file.requires
     if(file._likes.isJsLike){
-      alies[_file] = resFile.pkg ? pkg[resFile.pkg].uri : file.url
+      var fileId = _file.replace('.js','')
+      alies[fileId] = resFile.pkg ? pkg[resFile.pkg].uri : file.url
       // file.id = file.filename
       // file.moduleId = file.filename
-      if(requires.length <= 0)return deps[_file] = void 0;
-      deps[_file] = []
-      requires = resFile.pkg ? requires.filter(function(req,i){
-        if(!~pkg[resFile.pkg].has.indexOf(req))return req;
-      }) : requires
-      console.log(requires,'fffffffffffffffff')
-      if(requires.length <= 0)return deps[_file] = void 0;
+      if(requires.length <= 0)return deps[fileId] = void 0;
+      deps[fileId] = []
+
+      // requires = resFile.pkg ? requires.filter(function(req,i){
+      //   if(!~pkg[resFile.pkg].has.indexOf(req))return req;
+      // }) : requires
+      // console.log(requires,'fffffffffffffffff')
+      if(requires.length <= 0)return deps[fileId] = void 0;
       requires.forEach(function(req,i){
-        deps[_file].push(idsObj[req].url)
+        // deps[fileId].push(idsObj[req].url)
+        deps[fileId].push(req.replace('.js',''))
       })
     }
   })
@@ -62,20 +65,23 @@ fis.pcSub = function(){
   fis
     // .media('dev')
     .match('**',{//这里封锁所有输出，只有配置了的才能进行输出
-      deploy: fis.plugin('local-deliver',{
-        to: fis.get('outputDir')
-      }),
+      deploy: [
+        fis.plugin('local-deliver',{
+          to: fis.get('outputDir')
+        })
+      ],
       release: false,
       useHash: false
     })
     .match('**/(*).md',{
       rExt:'.html',
       release: '${namespace}/page/$1',
-      parser: fis.plugin('marked'),
+      parser: fis.plugin('marked')
     })
     .match('/page/(*).html',{
       parser: fis.plugin('handlebars',{
-        dataFile:'/_data.js'
+        dataFile:'/_data.js',
+        _data:fis.get('extendData') || {}
       }),
       useMap: true,
       release: '${namespace}/$1.html'
@@ -111,28 +117,62 @@ fis.pcSub = function(){
       //, postpackager:fis.plugin('loader',{allInOne:true})
     })
     // .match('static')
-    .match('lib/*.js',{
-      isMod: true,
-      useMap: true,
-      release: '${namespace}/static/lib.js',
-      packTo: '${namespace}/lib/lib.js',
-      optimizer: fis.plugin('uglify-js')
-    })
-    .match('*.{scss,sass,less,css}', {
-      optimizer: fis.plugin('clean-css')
-    })
-    .match('css/*.{scss,sass,less,css}',{
-      release: '${namespace}/static/index.css',
-      packTo: '${namespace}/css/index.css'
-    })
+    // .match('lib/*.js',{
+    //   isMod: true,
+    //   useMap: true,
+    //   release: '${namespace}/static/lib.js',
+    //   packTo: '${namespace}/lib/lib.js',
+    //   optimizer: fis.plugin('uglify-js')
+    // })
+    // .match('*.{scss,sass,less,css}', {
+    //   optimizer: fis.plugin('clean-css')
+    // })
+    // .match('css/*.{scss,sass,less,css}',{
+    //   release: '${namespace}/static/index.css',
+    //   packTo: '${namespace}/css/index.css'
+    // })
     .match('*.png', {
       optimizer: fis.plugin('png-compressor'),
       useSpriter: true
     })
-    .match('lib/lib.js',{
-      isMod: true,
-      useMap: true,
-      release: '${namespace}/static/lib.js'
-    })
+    // .match('lib/lib.js',{
+    //   isMod: true,
+    //   useMap: true,
+    //   release: '${namespace}/static/lib.js'
+    // })
 
+    fis
+      .media('upload')
+      .match('*.{scss,sass,less,css}', {
+        optimizer: fis.plugin('clean-css')
+      })
+      .match('css/*.{scss,sass,less,css}',{
+        release: '${namespace}/static/index.css',
+        packTo: '${namespace}/css/index.css'
+      })
+      .match('lib/*.js',{
+        isMod: true,
+        useMap: true,
+        release: '${namespace}/static/lib.js',
+        packTo: '${namespace}/lib/lib.js',
+        optimizer: fis.plugin('uglify-js')
+      })
+      .match('lib/lib.js',{
+        isMod: true,
+        useMap: true,
+        release: '${namespace}/static/lib.js'
+      })
+      .match('**',{
+        charset: 'gbk',
+        deploy: [
+          fis.plugin('encoding'),
+          fis.plugin('local-deliver',{
+            to: fis.get('outputDir')
+          }),
+          fis.plugin('http-push', {
+            receiver: fis.get('remoteServer')+'/receiver',
+            to: '/node-server/www/' // 注意这个是指的是测试机器的路径，而非本地机器
+          })
+        ]
+      })
 }
