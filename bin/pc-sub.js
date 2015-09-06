3c759b4a2dddd3d69146bae82a8406ce05390bb3
Jin_C <275727449@qq.com>
2015-09-06 09:45:02 +0800

    fix mac os
    fix mac os

--- bin/pc-sub.js
+++ bin/pc-sub.js
@@ -1,33 +1,1 @@
-#!/usr/bin/env node
-
-// vi foo/bin/pc-sub.js
-
-var Liftoff = require('liftoff');
-var argv = require('minimist')(process.argv.slice(2));
-var path = require('path');
-var cli = new Liftoff({
-  name: 'pc-sub', // 命令名字
-  processTitle: 'pc-sub',
-  moduleName: 'pc-sub',
-  configName: 'fis-conf',
-
-  // only js supported!
-  extensions: {
-    '.js': null
-  }
-});
-
-cli.launch({
-  cwd: argv.r || argv.root,
-  configPath: argv.f || argv.file
-}, function(env) {
-  var fis;
-  if (!env.modulePath) {
-    fis = require('../');
-  } else {
-    fis = require(env.modulePath);
-  }
-  fis.set('system.localNPMFolder', path.join(env.cwd, 'node_modules/pc-sub'));
-  fis.set('system.globalNPMFolder', path.dirname(__dirname));
-  fis.cli.run(argv, env);
-});
+#!/usr/bin/env node// vi foo/bin/pc-sub.jsvar Liftoff = require('liftoff');var argv = require('minimist')(process.argv.slice(2));var path = require('path');var cli = new Liftoff({  name: 'pc-sub', // 命令名字  processTitle: 'pc-sub',  moduleName: 'pc-sub',  configName: 'fis-conf',  // only js supported!  extensions: {    '.js': null  }});cli.launch({  cwd: argv.r || argv.root,  configPath: argv.f || argv.file}, function(env) {  var fis;  if (!env.modulePath) {    fis = require('../');  } else {    fis = require(env.modulePath);  }  fis.set('system.localNPMFolder', path.join(env.cwd, 'node_modules/pc-sub'));  fis.set('system.globalNPMFolder', path.dirname(__dirname));  fis.cli.run(argv, env);});
